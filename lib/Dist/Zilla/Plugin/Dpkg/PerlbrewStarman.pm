b1e6d214b20045032412951b918a6c609e3668a9
Cory G Watson <gphat@cpan.org>
2011-12-21 16:06:38 -0600

    Wrap _generate_file to fix starman_port. Hack!

--- lib/Dist/Zilla/Plugin/Dpkg/PerlbrewStarman.pm
+++ lib/Dist/Zilla/Plugin/Dpkg/PerlbrewStarman.pm
@@ -34,8 +34,6 @@ Starman.  It makes the following assumptions:
 
 =item Nginx config is in config/nginx/$packagename.conf
 
-=item Your $packagename is setup in the services file so it can be used as a port
-
 =item Your app can be preloaded
 
 =item Your app only listens locally (nginx handles the rest)
@@ -80,7 +78,7 @@ PIDFILE="/var/run/$APP.pid"
 
 PERLBREW_PATH="$APPDIR/perlbrew/bin"
 
-DAEMON_ARGS="-Ilib $PSGIAPP --daemonize --user $APPUSER --preload-app --workers 5 --pid $PIDFILE --port $APP --host 127.0.0.1 --error-log /var/log/$APP/error.log"
+DAEMON_ARGS="-Ilib $PSGIAPP --daemonize --user $APPUSER --preload-app --workers 5 --pid $PIDFILE --port {$starman_port} --host 127.0.0.1 --error-log /var/log/$APP/error.log"
 '
 );
 
@@ -373,5 +371,13 @@ has 'starman_port' => (
     isa => 'Str',
     required => 1
 );
+
+around '_generate_file' => sub {
+    my $orig = shift;
+    my $self = shift;
+    
+    $_[2]->{starman_port} = $self->starman_port;
+    $self->$orig(@_);
+}
 
 1;
