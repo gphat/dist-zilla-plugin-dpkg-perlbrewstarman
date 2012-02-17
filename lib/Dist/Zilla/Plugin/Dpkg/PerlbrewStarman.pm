dc6bd9744d045b31382d13b02213e99da847be08
Cory G Watson <gphat@onemogin.com>
2012-02-17 10:46:47 -0600

    Do syntax checking to fix restarts. Refs PRG-144.

--- lib/Dist/Zilla/Plugin/Dpkg/PerlbrewStarman.pm
+++ lib/Dist/Zilla/Plugin/Dpkg/PerlbrewStarman.pm
@@ -146,7 +146,7 @@ check_running() \{
 \}
 
 check_compile() \{
-  if ( cd $APPDIR ; perl -Ilib -M$APPLIB -ce1 ) ; then
+  if ( cd $APPLIB ; find -type f -name '*.pm' | xargs perl -c ) ; then
     return 1
   else
     return 0
